6b757639ac0ac0ea70196e3c16fa8ece4c292d70
Slayer <aksuplayz1337@gmail.com>
2021-09-21 14:18:02 +0300

    Fix empty

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const config = require('config').util.toObject();
 const mysql = require('@drivet/database')
 
-client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
+client.on('ready', () => console.log(`Logged in as ${client.user.tag}! ${process.env.NODE_ENV === 'development' ? '[DEVELOPMENT]' : ''}`));
 client.on('error', err => console.log(err));
 
 function detectURLs(message) {
@@ -14,28 +14,40 @@ function detectURLs(message) {
 
 client.on('messageCreate', async message => {
   try {
+    //&& message.author.id !== '235148962103951360'
     if (message.channel.id !== '875065636039049236' || message.author.id !== '515067662028636170') return
     const beemoUrl = detectURLs(message.embeds[0].description);
     const urlFormatted = beemoUrl[0].replace('**', '')
     console.log(urlFormatted)
   
     const request = await axios.get(urlFormatted).catch(err => { console.error('Failed to fetch Beemo AntiSpam logs.') })
-    let users = request.data.toString().substring(request.data.indexOf("Raw IDs:") + 10).replace(/\r\n/g,'\n').replace(/\s/g, '').split('\n')
+    let users = request.data.toString().substring(request.data.indexOf("Raw IDs:") + 10).replace(/\r\n/g,'\n').split('\n')
     
     message.channel.send('Reporting that ^^')
-  
-    for(let id of users) {
-      console.log(`${new Date()} - ${id}`)
-  
-      mysql.query('INSERT INTO reports SET ?', { author: '814952115424067664', reported: id, type: 'Spamming', reason: 'Attempted to raid a server', created_at: Date.now() })
-  
-      await axios.get('https://discord.riverside.rocks/report.json.php', {
-        params: {
-          id,
-          key: config.ddubToken,
-          details: `Attempted to raid a server`
-        }
-      }).catch(err => { console.log(`Failed to report ${id} to DDUB`) })
+
+    if (process.env.NODE_ENV === 'development') {
+      for (let id of users) {
+        if (id === '') return;
+        console.log(`- ${id}`)
+      }
+
+      return;
+    } else {
+      for(let id of users) {
+        if (id === '') return;
+
+        console.log(`${new Date()} - ${id}`)
+    
+        mysql.query('INSERT INTO reports SET ?', { author: '814952115424067664', reported: id, type: 'Spamming', reason: 'Attempted to raid a server', created_at: Date.now() })
+    
+        await axios.get('https://discord.riverside.rocks/report.json.php', {
+          params: {
+            id,
+            key: config.ddubToken,
+            details: `Attempted to raid a server`
+          }
+        }).catch(err => { console.log(`Failed to report ${id} to DDUB`) })
+      }
     }
   } catch (err) {
     console.log(err)
